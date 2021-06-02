df0d82d5ce6b81e7f9df17280199a45f7dc91e46
christnansi <manuelnansi26@gmail.com>
2021-06-02 20:17:51 +0530

    Added Cars app and fetched the data into homepage

--- cars/models.py
+++ cars/models.py
@@ -3,7 +3,9 @@ from datetime import datetime
 from ckeditor.fields import RichTextField
 from multiselectfield import MultiSelectField
 
+
 # Create your models here.
+
 
 class Car(models.Model):
 
@@ -63,7 +65,7 @@ class Car(models.Model):
 
     year_choice = []
     for r in range(2000, (datetime.now().year+1)):
-        year_choice.append((r,r))
+        year_choice.append((r, r))
 
     features_choices = (
         ('Cruise Control', 'Cruise Control'),
@@ -89,14 +91,13 @@ class Car(models.Model):
         ('6', '6'),
     )
 
-
     car_title = models.CharField(max_length=255)
-    state = models.CharField(choices=state_choice, max_length=100)
-    city = models.CharField(max_length=100)
-    color = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
+    state = models.CharField(choices=state_choice, max_length=50)
+    city = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
     year = models.IntegerField(('year'), choices=year_choice)
-    condition = models.CharField(max_length=100)
+    condition = models.CharField(max_length=50)
     price = models.IntegerField()
     description = RichTextField()
     car_photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
@@ -105,18 +106,19 @@ class Car(models.Model):
     car_photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     car_photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     features = MultiSelectField(choices=features_choices)
-    body_style = models.CharField(max_length=100)
-    engine = models.CharField(max_length=100)
-    transmission = models.CharField(max_length=100)
-    interior = models.CharField(max_length=100)
+    body_style = models.CharField(max_length=50)
+    engine = models.CharField(max_length=50)
+    transmission = models.CharField(max_length=50)
+    interior = models.CharField(max_length=50)
     miles = models.IntegerField()
-    door = models.CharField(choices=door_choices, max_length=100)
+    door = models.CharField(choices=door_choices, max_length=10)
     passengers = models.IntegerField()
-    vin_no = models.CharField(max_length=100)
+    vin_no = models.CharField(max_length=50)
     milage = models.IntegerField()
-    fuel_type = models.CharField(max_length=100)
-    no_of_owners = models.CharField(max_length=100)
+    fuel_type = models.CharField(max_length=50)
+    no_of_owners = models.IntegerField()
     is_featured = models.BooleanField(default=False)
-    created_date = models.DateTimeField(default=datetime.now(), blank=True)
+    created_date = models.DateTimeField(default=datetime.now, blank=True)
 
-
+    def __str__(self):
+        return self.car_title
--- carzone/settings.py
+++ carzone/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'pages',
     'cars',
     'ckeditor',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
